41d19b55b8a2efaae7951cfb2e68408ff1ac5172
Alison <aapeebles@gmail.com>
2025-01-30 21:36:29 +0000

    reorganiz sub queries into CTEs

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,6 +1,6 @@
 --Import CTEs
 with
-customers as (
+base_customers as (
     select * from {{ source('jaffle_shop', 'customers') }}
 ),
 orders as (
@@ -8,38 +8,30 @@ orders as (
 ),
 payments as (
     select * from {{ source('stripe', 'payment') }}
-)
+),
 
 -- Logical CTEs
-
--- Final CTEs
-select
-
-    orders.id as order_id,
-    orders.user_id as customer_id,
-    last_name as surname,
-    first_name as givenname,
-    first_order_date,
-    order_count,
-    total_lifetime_value,
-    round(amount/100.0,2) as order_value_dollars,
-    orders.status as order_status,
-    payments.status as payment_status
-
-from orders
-
-    join (
-      select 
+customers as(
+    select 
         first_name || ' ' || last_name as name, 
         * 
+    from base_customers
 
-      from customers
-) customers
-on orders.user_id = customers.id
-
-join (
-
+),
+a as (
     select 
+        row_number() over (partition by user_id order by order_date, id) as user_order_seq,
+        *
+    from orders
+),
+b as (
+         select 
+        first_name || ' ' || last_name as name,
+        *
+      from base_customers
+),
+customer_order_history as (
+   select 
         b.id as customer_id,
         b.name as full_name,
         b.last_name as surname,
@@ -80,19 +72,9 @@ join (
 
         array_agg(distinct a.id) as order_ids
 
-    from (
-      select 
-        row_number() over (partition by user_id order by order_date, id) as user_order_seq,
-        *
-      from orders
-    ) a
+    from a
 
-    join ( 
-      select 
-        first_name || ' ' || last_name as name,
-        *
-      from customers
-    ) b
+    join b
     on a.user_id = b.id
 
     left outer join payments c
@@ -102,10 +84,35 @@ join (
 
     group by b.id, b.name, b.last_name, b.first_name
 
-) customer_order_history
-on orders.user_id = customer_order_history.customer_id
+),
 
-left outer join payments
-on orders.id = payments.orderid
+-- Final CTEs
+final as (
+    select
 
-where payments.status != 'fail'
+        orders.id as order_id,
+        orders.user_id as customer_id,
+        last_name as surname,
+        first_name as givenname,
+        first_order_date,
+        order_count,
+        total_lifetime_value,
+        round(amount/100.0,2) as order_value_dollars,
+        orders.status as order_status,
+        payments.status as payment_status
+
+    from orders
+
+        join customers
+    on orders.user_id = customers.id
+
+    join customer_order_history
+    on orders.user_id = customer_order_history.customer_id
+
+    left outer join payments
+    on orders.id = payments.orderid
+
+    where payments.status != 'fail'
+)
+-- Simple select statement
+select * from final
