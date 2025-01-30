953c1850b98aa46e6f09449485d298d0f9132cfa
Alison <aapeebles@gmail.com>
2025-01-30 22:27:43 +0000

    move source and stg CTEs to sql model files

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,62 +1,32 @@
 --Import CTEs
 with
-base_customers as (
-    select * from {{ source('jaffle_shop', 'customers') }}
-),
-base_orders as (
-    select * from {{ source('jaffle_shop', 'orders') }}
-),
-base_payments as (
-    select * from {{ source('stripe', 'payment') }}
-),
 
--- Logical CTEs
-
---Staging
 customers as(
-    select 
-        id as customer_id
-        last_name as surname,
-        first_name as givenname,
-        first_name || ' ' || last_name as full_name
-    from base_customers
-
+    select * from {{ ref('stg_jaffle_shop__customers') }}
 ),
 orders as (
-    select 
-        id as order_id,
-        user_id as customer_id,
-        status as order_status,
-        row_number() over (partition by user_id order by order_date, id) as user_order_seq,
-        *
-    from base_orders
+    select * from {{ ref('stg_jaffle_shop__orders') }}
 ),
 payments as (
-    select 
-        id as payment_id
-        orderid as order_id
-        status as payment_status
-        round(payments.amount/100.0,2) as payment_amount
-    from base_payments
-)
+    select * from {{ ref('stg_stripe__payments') }}
+),
 
--- Marts
 customer_order_history as (
    select 
         customers.customer_id,
         customers.full_name,
         customers.surname,
         customers.givenname,
-        min(order_date) as first_order_date,
+        min(orders.order_date) as first_order_date,
 
         min(case 
             when orders.order_status not in ('returned','return_pending')
-            then order_date
+            then orders.order_date
         end) as first_non_returned_order_date,
 
         max(case 
             when orders.order_status not in ('returned','return_pending')
-            then order_date
+            then orders.order_date
         end) as most_recent_non_returned_order_date,
 
         coalesce(max(user_order_seq),0) as order_count,
