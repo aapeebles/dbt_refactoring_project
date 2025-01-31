3633ecad2f4d439c0882489af68f5512e5a501a9
Alison Peebles Madigan <aapeebles@gmail.com>
2025-01-31 22:52:57 +0000

    refactor to int model

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,18 +1,20 @@
 with 
 source as (
-    select * from {{ source('jaffle_shop', 'orders') }}
+    select * from {{ ref("orders") }}
 ),
 transformed as (
-    select 
+    
+     select
         id as order_id,
         user_id as customer_id,
         order_date as order_placed_at,
         status as order_status,
+
         case 
             when status not in ('returned','return_pending') 
-            then order_date
-        end as valid_order_date,
-        row_number() over (partition by user_id order by order_date, id) as user_order_seq
+            then order_date 
+        end as valid_order_date
+
     from source
 )
 select * from transformed
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,14 +1,16 @@
 
 with 
 source as (
-    select * from {{ source('stripe', 'payment') }}
+    select * from {{ ref("payments") }}
 ),
 transformed as (
     select 
         id as payment_id,
         orderid as order_id,
         status as payment_status,
+        created as payment_created_at,
         round(amount/100.0,2) as payment_amount
     from source
+    where status <> 'fail'
 )
 select * from transformed
